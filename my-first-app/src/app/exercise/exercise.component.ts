33e19d158a9a018ac92819ea49401a74b8b8c022
Amalia <74189129+chocolateprincess666@users.noreply.github.com>
2021-11-04 15:34:16 +0100

    updated exercise

--- my-first-app/src/app/exercise/exercise.component.ts
+++ my-first-app/src/app/exercise/exercise.component.ts
@@ -11,7 +11,8 @@ export class ExerciseComponent implements OnInit {
 
   onToggleDetails() {
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length + 1);
+    //this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
   constructor() { 
